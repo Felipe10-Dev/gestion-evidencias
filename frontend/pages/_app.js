289eb44acbe014c71547cd6460c71470052d8ea5
Vercel <vercel[bot]@users.noreply.github.com>
2026-03-23 21:27:41 +0000

    Install Vercel Web Analytics with latest docs
    This is an automated pull request generated by Vercel to help install [Web Analytics](https://vercel.com/docs/analytics) in your application.
    
    Co-authored-by: Vercel <vercel[bot]@users.noreply.github.com>

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -4,6 +4,7 @@ import { AuthProvider } from '@/context/auth/AuthContext'
 import { ToastProvider } from '@/context/toast/ToastContext'
 import { AppShell } from '@/components/layout/AppShell'
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Analytics } from '@vercel/analytics/next'
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -13,6 +14,7 @@ function MyApp({ Component, pageProps }) {
           <Component {...pageProps} />
         </AppShell>
         <SpeedInsights />
+        <Analytics />
       </ToastProvider>
     </AuthProvider>
   )
